5586b74f1d2b91f2de44d938643928b2363ef90e
Vinícius <vinicius.ferreira@ufpe.br>
2025-09-02 11:02:03 -0300

    adiciona scatterplot

--- eda.py
+++ eda.py
@@ -8,6 +8,7 @@ with app.setup:
     # que será importada deverá ser declarada aqui. 
     import streamlit as st
     import pandas as pd
+    import plotly.express as px
 
 
 @app.cell
@@ -24,9 +25,44 @@ def _(mo):
 
 
 @app.function
-@st.cache_data(ttl=10800, show_spinner="Carregando dados do Google Sheets...")
+@st.cache_data(show_spinner="Carregando os dados do arquivo...")
 def load_data():
     return pd.read_csv('data/athletes.csv')
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""# **Plots**""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## **Scatterplot**""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Relação entre peso do atleta (`weight`) e peso levantado no levantamento terra (`deadlift`). """)
+    return
+
+
+@app.function
+def generate_scatterplot(df):
+    fig = px.scatter(
+        df,
+        x="weight",
+        y="deadlift",
+        title="Weight vs Deadlift"
+    )
+
+    fig.update_layout(
+        xaxis_title="Body Weight (lbs)",
+        yaxis_title="Deadlift (lbs)"
+    )
+
+    return fig
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from eda import load_data
+from eda import load_data, generate_scatterplot
 
 # Page config
 st.set_page_config(page_title='Crossfir Data')
 
 def main():
     df = load_data()
-    st.dataframe(df)
+    st.plotly_chart(generate_scatterplot(df))
+    with st.expander('Raw Data'):
+        st.dataframe(df)
 
 if __name__ == "__main__":
     main()
