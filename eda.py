a0381749de8a12e51877b16ef575fa49c9957bfa
Vinícius <vinicius.ferreira@ufpe.br>
2025-09-22 08:36:10 -0300

    melhora limpeza de dados

--- eda.py
+++ eda.py
@@ -38,13 +38,13 @@ def _(mo):
 
 @app.function
 @st.cache_data()
-def clean_data_for_scatterplot(df):
-    filtered_df = df.dropna(subset=["weight", "deadlift"])
+def clean_data_for_scatterplot(df, sample_size, x_axis, y_axis, x_thresholds, y_thresholds):
+    filtered_df = df.dropna(subset=[x_axis, y_axis])
     no_outliers_df = filtered_df[
-        (filtered_df["weight"] <= 500) & (filtered_df["deadlift"] <= 1000) &
-        (filtered_df["weight"] >= 100) & (filtered_df["deadlift"] >= 10)
+        (filtered_df[x_axis] <= x_thresholds[1]) & (filtered_df[y_axis] <= y_thresholds[1]) &
+        (filtered_df[x_axis] >= x_thresholds[0]) & (filtered_df[y_axis] >= y_thresholds[0])
     ]
-    return no_outliers_df.head(1000)
+    return no_outliers_df.head(sample_size)
 
 
 @app.cell
@@ -68,7 +68,7 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Relação entre peso do atleta (`weight`) e peso levantado no levantamento terra (`deadlift`).""")
+    mo.md(r"""Relação entre duas variáveis com linha de tendência.""")
     return
 
 
@@ -89,6 +89,19 @@ def generate_scatterplot(df, x_axis="weight", y_axis="deadlift", trendline="ols"
         yaxis_title=y_axis
     )
 
+    return fig
+
+
+@app.function
+def generate_histogram(df, column):
+    fig = px.histogram(
+        df,
+        x=column,
+        nbins=50, 
+        opacity=0.7, 
+        title=f'Ditribution of {column}'
+    )
+    
     return fig
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from eda import load_data, clean_data_for_scatterplot, generate_scatterplot
+from eda import load_data, clean_data_for_scatterplot, generate_scatterplot, generate_histogram
 
 # Page config
 st.set_page_config(page_title='Crossfir Data')
@@ -25,6 +25,14 @@ def main():
             key='y_axis'
         )
         # Seleciona modelo de regressão
+        x_threshold_lower =  st.number_input(f'Select lower bound to {x_axis}', 0, 1000, 0, 1) # min, max, default, step
+        x_threshold_upper =  st.number_input(f'Select upper bound to {x_axis}', 0, 1000, 1000, 1) # min, max, default, step
+        y_threshold_lower = st.number_input(f'Select lower bound to {y_axis}', 0, 1000, 0, 1) # min, max, default, step
+        y_threshold_upper = st.number_input(f'Select upper bound to {y_axis}', 0, 1000, 1000, 1) # min, max, default, step
+
+        x_thresholds = (x_threshold_lower, x_threshold_upper)
+        y_thresholds = (y_threshold_lower, y_threshold_upper)
+
         trendline_options = {
             'Ordinary Least Squares': 'ols',
             'Expanding': 'expanding',
@@ -37,10 +45,21 @@ def main():
         )
         trendline = trendline_options[trendline_display]
 
+        # Seleciona tamanho da amostra
+        sample_size = st.radio('Select sample size', options=[1000, 10000, 100000])
+
     # Scatterplot
-    df = clean_data_for_scatterplot(df)
-    st.plotly_chart(generate_scatterplot(df, x_axis, y_axis, trendline))
-    # Scatterplot stats
+    df = clean_data_for_scatterplot(df, sample_size, x_axis=x_axis, y_axis=y_axis, x_thresholds=x_thresholds, y_thresholds=y_thresholds)
+
+    scatter_tab, hist_tab = st.tabs(['Scatter', 'Histogram'])
+
+    with scatter_tab:
+        st.plotly_chart(generate_scatterplot(df, x_axis, y_axis, trendline))
+    with hist_tab:
+        st.plotly_chart(generate_histogram(df, x_axis), theme=None)
+        st.plotly_chart(generate_histogram(df, y_axis), theme='streamlit')
+
+    # Stats
     if st.checkbox('Show Statistcs'):
         st.subheader('Averages')
         st.markdown(f'**{x_axis}**: {df[x_axis].mean():.2f}')
